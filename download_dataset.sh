9e2841666b3654bebeb468d919899b03652992e6
Riccardo Balbo <riccardo0blb@gmail.com>
2022-09-22 17:13:16 +0200

    convert to PNG, use default res of 256x

--- download_dataset.sh
+++ download_dataset.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 collection="$1"
-conda activate ldm || true
+if [ "$RES" = "" ];
+then
+	export RES="256x256"
+fi
 mkdir -P ~/datasets
 python3 ~/pinterest-downloader/pinterest-downloader.py $collection -io -d ~/datasets
-mogrify -geometry 512x512 ~/datasets/$collection/*.jpg
-mogrify -geometry 512x512 ~/datasets/$collection/*.png
-rm ~/datasets/$collection/*~
-rm ~/datasets/$collection/*.log
-rm ~/datasets/$collection/*.urls
+mogrify -geometry $RES ~/datasets/$collection/*.jpg
+mogrify -geometry $RES ~/datasets/$collection/*.png
+rm ~/datasets/$collection/*~ || true
+rm ~/datasets/$collection/*.log || true
+rm ~/datasets/$collection/*.urls || true
+mogrify -format png ~/datasets/$collection/*.*
+rm ~/datasets/$collection/*.jpg || true
