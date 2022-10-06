4fe6972e01278593eb5260aa2a08f91601344cd4
Riccardo Balbo <riccardo0blb@gmail.com>
2022-10-06 09:24:24 +0200

    do not download if model already exists, symlink for webui

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,12 +7,20 @@ mkdir -p /data/models
 mkdir -p /data/datasets
 mkdir -p /data/models/custom
 
-
-aria2c --seed-time=0 \
-"magnet:?xt=urn:btih:3a4a612d75ed088ea542acac52f9f45987488d1c&dn=sd-v1-4.ckpt&tr=udp%3a%2f%2ftracker.openbittorrent.com%3a6969%2fannounce&tr=udp%3a%2f%2ftracker.opentrackr.org%3a1337" \
--d /data/models/
+if [ ! -f /data/models/model.ckpt ];
+then
+    aria2c --seed-time=0 \
+    "magnet:?xt=urn:btih:3a4a612d75ed088ea542acac52f9f45987488d1c&dn=sd-v1-4.ckpt&tr=udp%3a%2f%2ftracker.openbittorrent.com%3a6969%2fannounce&tr=udp%3a%2f%2ftracker.opentrackr.org%3a1337" \
+    -d /data/models/
+fi
 
 ln -s /data/models/sd-v1-4.ckpt /data/models/model.ckpt 
+
+mkdir -p /data/models/Stable-diffusion/
+if [ ! -f /models/Stable-diffusion/sd-v1-4.ckpt ];
+then
+    ln -s /data/models/sd-v1-4.ckpt /models/Stable-diffusion/sd-v1-4.ckpt
+fi
 
 # if [ ! -f /models/model.ckpt ];
 # then
